a68fb07f5bab9836493fd6a5bfaf93d9930a5b71
aasthashrimal <aasthashrimal22@gmail.com>
2025-06-18 14:44:38 +0530

    Update Problem2.java

--- StackProblems/Problem2.java
+++ StackProblems/Problem2.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Problem2{
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Stack<Integer> stack = new Stack<>();
-        
+        Scanner sc = new Scanner(System.in);        
         System.out.print("Enter the postfix expression: ");
         char[] exp = sc.next().toCharArray();                // String array can be used for more than one digit integers
+        evaluatePostfix(exp); 
+        sc.close();       
+    }
 
+     public static void evaluatePostfix(char[] exp){
+        Stack<Integer> stack = new Stack<>();
         int res = 0;
         for(char c : exp){
 
@@ -22,7 +26,7 @@ public class Problem2{
             else {
                 if(stack.size()<2){
                     System.out.println("Invalid expression");
-                    sc.close(); System.exit(0);
+                    System.exit(0);
                 }
 
                 int a = stack.pop(); 
@@ -35,22 +39,23 @@ public class Problem2{
                     case '/' -> { 
                         if(a==0) {
                         System.out.println("Divison by zero error"); 
-                        sc.close();
                         System.exit(0);
                         }
 
                         res = b/a;
                     }
 
-                    default -> System.out.println("Invalud character in expression");
+                    default -> {System.out.println("Invalid character in expression");
+                                return;}
                 }
+                 stack.push(res);
             }
         }
 
         if(stack.size()==1){
-            System.out.println("Result of the given postfix expression is "+res);
+            System.out.println("Result of the given postfix expression is "+stack.pop());
         } else{
             System.out.println("Invalid expression");
         }
-    }
+     }
 }

5a17496f3d0de6016ede56f960eeacb51ca89aa5
aasthashrimal <aasthashrimal22@gmail.com>
2025-07-01 17:01:51 +0530

    Add files via upload

---
+++ QueueProblems/Problem1.java
@@ -0,0 +1,41 @@
+/* Given an integer k and a queue of integers, we need to reverse the order of the first k elements of the queue, 
+leaving the other elements in the same relative order.  
+ "If the size of queue is smaller than the given k , then return the original queue." */
+
+import java.util.*;
+
+public class Problem1 {
+
+    public static Queue<Integer> reverse(Queue<Integer> queue, int k){
+        if(queue.size()<k) return queue;
+
+        Stack<Integer> s = new Stack<>();
+        for(int i =0; i<k; i++){
+            s.push(queue.poll());
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        while(!s.isEmpty()){
+            q.offer(s.pop());
+        }
+
+        while(!queue.isEmpty()){
+            q.offer(queue.poll());
+        }
+
+        return q;
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+        q.offer(40);
+        q.offer(50);
+        
+        q = reverse(q, 3);
+        while(!q.isEmpty()){
+            System.out.print(q.poll()+" ");
+        }
+    }
+}
